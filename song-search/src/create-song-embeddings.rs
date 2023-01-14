973750c68af24c0dc1c9f81e93712ae4f795daf7
Himanshu Neema <himanshun.iitkgp@gmail.com>
2023-01-13 17:39:17 -0800

    Add create-song-embeddings bin

--- song-search/src/create-song-embeddings.rs
+++ song-search/src/create-song-embeddings.rs
@@ -1,72 +1,102 @@
+use std::{path::PathBuf, sync::Arc, time::Duration};
+
 use anyhow::Result;
-use async_openai::{
-    types::{CreateEmbeddingRequestArgs, Embedding},
-    Client,
-};
-use song_search::{Song, SongEmbedding};
+use async_openai::Client;
+use backoff::ExponentialBackoffBuilder;
+use song_search::Song;
+use sqlx::postgres::PgPoolOptions;
+use tokio::sync::Semaphore;
+use tracing::{error, info};
+use tracing_subscriber::{fmt, prelude::*, EnvFilter};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let song_paths = [
-        "./data/input/ColdPlay.csv",
-        "./data/input/EdSheeran.csv",
-        "./data/input/JustinBieber.csv",
-        "./data/input/SelenaGomez.csv",
-    ];
-    let song_embeddings_path = "./data/output/song_embedding.yaml";
+    std::env::set_var("RUST_LOG", "info,sqlx=off,async_openai=warn"); // ideally set outside the program
+
+    // Setup Logging
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(EnvFilter::from_default_env())
+        .init();
 
     // 1. Create OpenAI Client
-    let openai_client = Client::new();
+    let backoff = ExponentialBackoffBuilder::new()
+        .with_initial_interval(Duration::from_secs(15))
+        .with_randomization_factor(0.1)
+        .with_multiplier(3.0)
+        .build();
 
-    // 2. Read all songs from csv file
-    let mut songs: Vec<Song> = vec![];
-    for path in song_paths {
-        let mut s = Song::get_songs(path)?;
-        println!("Read {} songs at {}", s.len(), path);
-        songs.append(&mut s);
-    }
-    println!("Total song count: {}", songs.len());
+    let openai_client = Client::new().with_backoff(backoff);
 
-    let mut output: Vec<SongEmbedding> = vec![];
+    // 2. Create PgPool for DB
+    let pg_pool = PgPoolOptions::new()
+        .max_connections(1500)
+        .connect("postgres://async-openai:async-openai@localhost:5432/songs")
+        .await?;
 
-    // 3. Make OpenAI call to get embedding for each song and add it to output
-    for song in songs {
-        let response = openai_client
-            .embeddings()
-            .create(
-                CreateEmbeddingRequestArgs::default()
-                    .input(song.embedding_text())
-                    .model("text-embedding-ada-002")
-                    .build()?,
-            )
-            .await?;
+    // 3. Create DB resources
+    Song::create_db_resources(&pg_pool).await?;
 
-        // Tokens used by this Song
-        println!(
-            "Song {} by {} used {} tokens",
-            &song.title, &song.artist, response.usage.total_tokens
+    // 4. Get all paths for all Artist data files
+    let artist_files: Vec<PathBuf> = std::fs::read_dir("./data/input")?
+        .map(|entry| entry.unwrap())
+        .map(|entry| entry.path())
+        .collect();
+
+    // 5. Process each Artist file
+    let mut total_songs = 0;
+    let mut handles = vec![];
+    // Only allow 5 tasks at a time
+    let semaphore = Arc::new(Semaphore::new(5));
+    for artist_file in artist_files {
+        let songs = Song::get_songs(&artist_file)?;
+        total_songs += songs.len();
+        info!(
+            "Processing songs {} in {}",
+            songs.len(),
+            artist_file.display()
         );
+        for song in songs {
+            let client = openai_client.clone();
+            let pool = pg_pool.clone();
+            // Only allow limit in-flight request at a time to "rate limit" API and DB calls and limit spawned tasks.
+            let permit = semaphore.clone().acquire_owned().await?;
+            let handle = tokio::spawn(async move {
+                // 6. If song doesn't exist in DB, get its
+                //    embedding and save it to DB
+                if sqlx::query(
+                    "SELECT * FROM songs WHERE artist = $1 AND title = $2 AND album = $3 LIMIT 1",
+                )
+                .bind(song.artist.clone())
+                .bind(song.title.clone())
+                .bind(song.album.clone())
+                .fetch_one(&pool)
+                .await
+                .is_err()
+                {
+                    info!(
+                        "Getting and saving embedding for {} by {}",
+                        &song.title, &song.artist
+                    );
 
-        // only one embedding expected
-        let embedding: Embedding = response.data.into_iter().nth(0).unwrap();
-
-        // 4. Create representation to save in the file
-        let song_embedding = SongEmbedding {
-            artist: song.artist,
-            album: song.album,
-            title: song.title,
-            embedding: embedding.embedding,
-        };
-
-        output.push(song_embedding);
+                    let _ = song
+                        .get_and_save_embedding(&client, &pool)
+                        .await
+                        .map_err(|e| {
+                            error!(
+                                "Failed to get and save embedding for {} by {}: {e}",
+                                song.title, song.artist
+                            )
+                        });
+                }
+                drop(permit);
+            });
+            handles.push(handle);
+        }
     }
-
-    // 5. Save all embeddings to a file
-    let yaml_file = std::fs::OpenOptions::new()
-        .write(true)
-        .create(true)
-        .open(song_embeddings_path)?;
-    serde_yaml::to_writer(yaml_file, &output)?;
+    info!("Total Songs: {total_songs}");
+    futures::future::join_all(handles).await;
+    info!("Done");
 
     Ok(())
 }
--- song-search/src/lib.rs
+++ song-search/src/lib.rs
@@ -1,9 +1,13 @@
 use std::{fmt::Display, path::Path};
 
 use anyhow::Result;
-use serde::{Deserialize, Serialize};
+use async_openai::{types::CreateEmbeddingRequestArgs, Client};
+use serde::Deserialize;
+use sqlx::{PgPool, Row};
+use tracing::info;
 
 pub const EMBEDDING_DIMENSION: usize = 1536;
+pub const EMBEDDING_MODEL: &str = "text-embedding-ada-002";
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "PascalCase")]
@@ -14,21 +18,40 @@ pub struct Song {
     pub lyric: String,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct SongEmbedding {
-    pub artist: String,
-    pub title: String,
-    pub album: String,
-    pub embedding: Vec<f32>,
-}
-
-impl Display for SongEmbedding {
+impl Display for Song {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "\t{} by {}", self.title, self.artist)
+        write!(f, "{} by {} / {}", self.title, self.artist, self.album,)
     }
 }
 
 impl Song {
+    /// Create extension, table and index
+    pub async fn create_db_resources(pg_pool: &PgPool) -> Result<()> {
+        // Create Extension
+        let extension_query = "CREATE EXTENSION IF NOT EXISTS vector;";
+        sqlx::query(extension_query).execute(pg_pool).await?;
+
+        // Create Table
+        let table_query = r#"CREATE TABLE IF NOT EXISTS songs
+(
+    artist text,
+    title text,
+    album text,
+    lyric text,
+    embedding vector(1536)
+);"#;
+
+        sqlx::query(table_query).execute(pg_pool).await?;
+
+        // Create Index
+        let index_query =
+            "CREATE INDEX IF NOT EXISTS songs_idx ON songs USING ivfflat (embedding vector_cosine_ops);";
+
+        sqlx::query(index_query).execute(pg_pool).await?;
+
+        Ok(())
+    }
+
     //// Read csv at given path into Vec<Song>
     pub fn get_songs<P: AsRef<Path>>(path: P) -> Result<Vec<Song>> {
         let mut rdr = csv::Reader::from_path(path)?;
@@ -40,15 +63,78 @@ impl Song {
         Ok(songs)
     }
 
-    /// Append artist + title + album + lyrics and trim it to length of 8192
+    /// Append artist + title + album + lyrics
     pub fn embedding_text(&self) -> String {
         [&self.artist, &self.title, &self.album, &self.lyric]
             .map(|s| s.to_owned())
             .join(" ")
             .replace("\n", " ")
+            .trim()
             .to_lowercase()
-        //.chars()
-        //.take(8192)
-        //.collect()
+    }
+
+    /// Get and save embedding
+    pub async fn get_and_save_embedding(&self, client: &Client, pg_pool: &PgPool) -> Result<()> {
+        let response = client
+            .embeddings()
+            .create(
+                CreateEmbeddingRequestArgs::default()
+                    .input(self.embedding_text())
+                    .model(EMBEDDING_MODEL)
+                    .build()?,
+            )
+            .await?;
+
+        info!(
+            "Song {} by {} used {} tokens",
+            self.title, self.artist, response.usage.total_tokens
+        );
+
+        // Insert embedding into DB
+        let pgvector = pgvector::Vector::from(response.data[0].embedding.clone());
+
+        sqlx::query("INSERT INTO songs (artist, title, album, lyric, embedding) VALUES ($1, $2, $3, $4, $5)")
+            .bind(self.artist.clone())
+            .bind(self.title.clone())
+            .bind(self.album.clone())
+            .bind(self.lyric.clone())
+            .bind(pgvector) // only single embedding expected
+            .execute(pg_pool)
+            .await?;
+
+        Ok(())
+    }
+
+    /// Search nearest embeddings using given query
+    pub async fn query(query: &str, n: i8, client: &Client, pg_pool: &PgPool) -> Result<Vec<Song>> {
+        // Get embedding from OpenAI
+        let response = client
+            .embeddings()
+            .create(
+                CreateEmbeddingRequestArgs::default()
+                    .input(query)
+                    .model(EMBEDDING_MODEL)
+                    .build()?,
+            )
+            .await?;
+
+        // Search for nearest neighbors in database
+        let pgvector = pgvector::Vector::from(response.data[0].embedding.clone());
+
+        Ok(sqlx::query(
+            "SELECT artist, title, album, lyric FROM songs ORDER BY embedding <-> $1 LIMIT $2::int",
+        )
+        .bind(pgvector) // only single embedding expected
+        .bind(n)
+        .fetch_all(pg_pool)
+        .await?
+        .into_iter()
+        .map(|r| Song {
+            artist: r.get("artist"),
+            title: r.get("title"),
+            album: r.get("album"),
+            lyric: r.get("lyric"),
+        })
+        .collect())
     }
 }
